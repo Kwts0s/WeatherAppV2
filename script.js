098a3aebf78c6aab758d6f9cb984dcf8fbf7eff5
Kwts0s <zeus.primers@gmail.com>
2023-02-22 00:30:08 +0200

    js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const currentTempEl = document.getElementById("current-temp");
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
-const API_KEY = '95dbec0fc97967ccaf9194c066f8cebd';
+const API_KEY = '021a30567af233a759c38b905002b794';
 
 setInterval(() => {
     const time = new Date();
@@ -18,11 +18,11 @@ setInterval(() => {
     const date = time.getDate();
     const day = time.getDay();
     const hour = time.getHours();
-    const hoursIn12HrFormat = hour >= 13 ? hour %12 :hour;
+    const hoursIn12HrFormat = hour >= 13 ? hour %12 :hour
     const minutes = time.getMinutes();
-    const ampm = hour >=12 ? 'PM' : 'AM';
+    const ampm = hour >=12 ? 'PM' : 'AM'
 
-    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' '+`<span id="am-pm">${ampm}</span>`;
+    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' '+`<span id="am-pm">${ampm}</span>`
 
     dateEl.innerHTML = days[day]+ ', '+date+' '+months[month];
 
@@ -33,9 +33,10 @@ function getWeatherData(){
     navigator.geolocation.getCurrentPosition((success)=>{
         console.log(success);
 
-        let{latitude , longitude} = success.coords;
+        let {latitude , longitude} = success.coords;
 
-        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`).then(res => res.json()).then(data => {console.log(data)
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`).then(res => res.json()).then(data => 
+        {console.log(data)
         })
     })
 };
